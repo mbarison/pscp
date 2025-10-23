6acc4f39c212645a6bcf501c48d11b11c6825ab4
Marcello Barisonzi <eltorpe@hotmail.com>
2025-10-22 23:11:22 -0400

    test with MS contracts

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests, re
-#import pandas as pd
+import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from html import unescape
+from datetime import datetime
+from time import sleep
 
 def unpack_struct(x):
     # first three rows contain URL, value, date
@@ -19,7 +22,7 @@ def unpack_struct(x):
 
     _dct["url"] = urljoin("https://search.open.canada.ca", header[0].find("a")["href"])
     _dct["vendor"] = header[0].find("a").text.strip()
-    _dct["contract_date"] = ptn_ctr_dat.findall(header[1].text.replace("\n", ""))[0].strip()
+    _dct["contract_date"] = datetime.strptime(ptn_ctr_dat.findall(header[1].text.replace("\n", ""))[0].strip(), "%b %d, %Y")
     _dct["total_value"] = ptn_tot_val.findall(header[0].text)[0].strip()
     _dct["original_value"] = ptn_ori_val.findall(header[2].text)[0].strip()
 
@@ -40,12 +43,14 @@ def unpack_struct(x):
 
     return _dct
 
-def main():
-    print("Hello from pscp!")
+def process_url(url):
 
-    url = "https://search.open.canada.ca/contracts/?owner_org=statcan&page=1&sort=contract_date+desc&search_text=microsoft"
+    print(f"Parsing {url}")
 
     r = requests.get(url)
+
+    raw_data = []
+    next_url = None
 
     if r.ok:
         #print(r.text)
@@ -56,19 +61,40 @@ def main():
 
         print(f"Found {len(contract_structs)} contracts.")
 
-        raw_data = []
-
         for i in contract_structs:
             d = unpack_struct(i)
             print(d)
             raw_data.append(d)
 
-        #df = pd.DataFrame(raw_data)
+        # check if there is a next url
+        n_u = bs.find("li", attrs={"class": "next"})
+        if n_u:
+            next_url = unescape(n_u.find("a")["href"])
 
-        #print(df)
+            # last page points to itself, discard
+            if "http" not  in next_url:
+                next_url = None
 
     else:
         print(r.status_code)
 
+    return (raw_data, next_url)
+
+def main(first_url):
+    print("Hello from pscp!")
+
+    next_url = first_url
+
+    raw_data = []
+
+    while next_url is not None:
+        r_d, next_url = process_url(next_url)
+        raw_data.extend(r_d)
+        sleep(1)
+
+    df = pd.DataFrame(raw_data)
+
+    df.to_parquet("ms_test.parquet", index=False)
+
 if __name__ == "__main__":
-    main()
+    main("https://search.open.canada.ca/contracts/?owner_org=statcan&page=1&sort=contract_date+desc&search_text=microsoft")
