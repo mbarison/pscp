9c2a278495285ac667dcd17e7f1706f713a839c9
Marcello Barisonzi <eltorpe@hotmail.com>
2025-10-22 22:45:58 -0400

    unpack all data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests, re
-import pandas as pd
+#import pandas as pd
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -11,7 +11,6 @@ def unpack_struct(x):
     ptn_tot_val = re.compile(r"Total Value:\s+\$(.+)$")
     ptn_ori_val = re.compile(r"Original Value:\s+\$(.+)$")
     
-
     _dct = {
             #"h0": header[0].text,
             #"h1": header[1].text,
@@ -21,10 +20,24 @@ def unpack_struct(x):
     _dct["url"] = urljoin("https://search.open.canada.ca", header[0].find("a")["href"])
     _dct["vendor"] = header[0].find("a").text.strip()
     _dct["contract_date"] = ptn_ctr_dat.findall(header[1].text.replace("\n", ""))[0].strip()
-    _dct["tot_value"] = ptn_tot_val.findall(header[0].text)[0].strip()
-    _dct["ori_value"] = ptn_ori_val.findall(header[2].text)[0].strip()
+    _dct["total_value"] = ptn_tot_val.findall(header[0].text)[0].strip()
+    _dct["original_value"] = ptn_ori_val.findall(header[2].text)[0].strip()
 
-    
+    # now unpack the rest
+    _dct["description"] = x.find("div", attrs={"aria-labelledby": "org_value_lbl"}).text.strip()
+    _dct["instrument_type"] = x.find("div", attrs={"aria-labelledby": "instrument_lbl"}).text.strip()
+    _dct["commodity_type"] = x.find("div", attrs={"aria-labelledby": "commodity_lbl"}).text.strip()
+
+    comm_block = x.find("div", attrs={"aria-labelledby": "comments_lbl"})
+    if comm_block:
+        _dct["comments"] = comm_block.text.strip()
+
+    add_comm_block = x.find("div", attrs={"aria-labelledby": "acomments_lbl"})
+    if add_comm_block:
+        _dct["comments"] = add_comm_block.text.strip()  
+
+    _dct["organization"] = x.find("div", attrs={"aria-labelledby": "org_lbl"}).text.strip()
+
     return _dct
 
 def main():
@@ -50,9 +63,9 @@ def main():
             print(d)
             raw_data.append(d)
 
-        df = pd.DataFrame(raw_data)
+        #df = pd.DataFrame(raw_data)
 
-        print(df)
+        #print(df)
 
     else:
         print(r.status_code)
